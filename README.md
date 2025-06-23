5cc52ebba73359264341132a6a24d61de8dff3f5
unknown <sadindore@gmail.com>
2025-06-23 21:54:22 +0530

    Initial commit - added blog website

---
+++ README.md
@@ -0,0 +1,57 @@
+# 🌐 My Awesome Blog Website
+
+A responsive blog website built using **HTML**, **CSS**, and **JavaScript**.  The blog features dynamic filtering of posts using a **search bar** filter by **categories**, **tags** and **title**. It includes **Dark** and **Light** mode to change the website theme with comment option. Designed for performance and readability.
+
+---
+
+## 📸 Preview
+
+![Blog Preview](./assets/images/Blog-Website-dark-mode.png) and (./assets/images/Blog-Website-light-mode.png)
+
+---
+
+## 🛠️ Technology used
+
+- **HTML** – Structure
+- **CSS** – Styling and responsive design
+- **JavaScript** – Interactivity (search filter, category/tags logic, comments)
+
+---
+
+## 🚀 Features
+
+- Responsive blog layout
+- Category & tag filtering
+- Search bar functionality
+- Comment section with local storage
+- Dark/light mode toggle
+
+---
+
+## 📁 Folder Structure
+/blog-website
+│
+├── index.html 
+├── styles.css
+├── script.js
+├── assets/
+│ └── images
+|   └── all images'
+├── posts
+| └── post1.html
+| └── post2.html
+| └── post3.html
+|     
+└── readme.md
+
+---
+
+## 🌐 Open in Browser
+Just open the index.html file in your browser.
+
+---
+
+## 🙋‍♀️ Author
+Shalini Gupta
+[LinkedIn](https://www.linkedin.com/in/shalini-gupta-8614877b/) 
+[GitHub] 
---
+++ script.js
@@ -0,0 +1,100 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // Mobile menu trigger 
+ const icon = document.getElementById("menuToggle");
+ const mobileMenu = document.getElementById("mobileMenu");
+
+  icon.addEventListener("click", () => {
+   // alert();
+    mobileMenu.classList.toggle("show");
+   icon.classList.toggle("show");
+   document.body.classList.toggle('no-scroll');  
+  });
+
+
+// Light/Dark Mode
+const toggle = document.getElementById('themeToggle');
+  const currentTheme = localStorage.getItem('theme');
+
+  // Load saved theme
+  if (currentTheme === 'dark') {
+    document.body.classList.add('dark');
+    toggle.checked = true;
+  }
+
+  toggle.addEventListener('change', () => {
+    if (toggle.checked) {
+      document.body.classList.add('dark');
+      localStorage.setItem('theme', 'dark');
+    } else {
+      document.body.classList.remove('dark');
+      localStorage.setItem('theme', 'light');
+    }
+  });
+
+
+  //  Post comments
+
+const commentForm = document.getElementById("commentForm");
+const commentsContainer = document.getElementById("commentList");
+const postId = document.body.dataset.postId;
+
+// Save to localStorage
+function saveComment(comment) {
+  const comments = JSON.parse(localStorage.getItem("comments")) || [];
+  comments.push(comment);
+  localStorage.setItem("comments", JSON.stringify(comments));
+}
+
+
+// Handle form submission
+commentForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const username = document.getElementById("username").value.trim();
+  const commentText = document.getElementById("message").value.trim();
+
+  if (username && commentText) {
+    const comment = { username, text: commentText };
+    addCommentToDOM(comment);
+    saveCommentForPost(postId, comment);
+
+    // Clear input fields
+    document.getElementById("username").value = "";
+    document.getElementById("message").value = "";
+  }
+});
+
+// Get comments using post id
+function getCommentsForPost(id) {
+  const allComments = JSON.parse(localStorage.getItem("commentsByPost")) || {};
+  return allComments[id] || [];
+}
+
+// Save comment using postid
+function saveCommentForPost(id, comment) {
+  const allComments = JSON.parse(localStorage.getItem("commentsByPost")) || {};
+  allComments[id] = allComments[id] || [];
+  allComments[id].push(comment);
+  localStorage.setItem("commentsByPost", JSON.stringify(allComments));
+}
+
+
+// Load saved comments from localStorage using postid
+window.addEventListener("DOMContentLoaded", () => {
+  const savedComments = getCommentsForPost(postId);
+  savedComments.forEach(addCommentToDOM);
+});
+ 
+// Render to page
+function addCommentToDOM({ username, text }) {
+  const commentElement = document.createElement("div");
+  commentElement.classList.add("comment");
+  commentElement.innerHTML = `
+    <h4>${username}</h4>
+    <p>${text}</p>
+  `;
+  commentsContainer.appendChild(commentElement);
+}
+  
+ });
+  
